eeade16e24053650a5af9214c64a0fcb9048f74f
Firdous Ali <firdous.ali.tp@gmail.com>
2024-01-24 19:57:26 +0500

    exp with absolute

--- components/TestFlexbox.js
+++ components/TestFlexbox.js
@@ -1,4 +1,11 @@
-import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 
 const TestFlexbox = () => {
@@ -8,30 +15,40 @@ const TestFlexbox = () => {
         backgroundColor: 'blue',
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'space-around', //vertical
-        alignItems: 'center', //horizontal
       }}>
-      <View
+      <View style={{flex: 4, backgroundColor: 'red'}}>
+        <View
+          style={{margin: 10, backgroundColor: 'white', flexDirection: 'row'}}>
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: 'blue',
+            }}></View>
+          <View style={{flex: 1, backgroundColor: 'grey'}}>
+            <Text>Name</Text>
+            <Text>Contact</Text>
+            <Text>Education</Text>
+          </View>
+        </View>
+      </View>
+      <View style={{flex: 6, backgroundColor: 'pink'}}></View>
+
+      <TouchableOpacity
         style={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'orange',
-          margin: 5,
-        }}></View>
-      <View
-        style={{
-          width: 100,
-          height: 100,
+          position: 'absolute',
+          left: 10,
+          top: 300,
           backgroundColor: 'green',
-          margin: 5,
-        }}></View>
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'red',
-          margin: 5,
-        }}></View>
+          width: 70,
+          height: 70,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        title="Chat"
+        onPress={() => {}}>
+        <Text>Chat</Text>
+      </TouchableOpacity>
     </View>
   );
 };
