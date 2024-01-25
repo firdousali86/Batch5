27ebda35a02aa6db9effde0593c4215a3cf41243
Firdous Ali <firdous.ali.tp@gmail.com>
2024-01-25 20:30:32 +0500

    exp with react navigation

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity, Button} from 'react-native';
 import {useState} from 'react';
 import UserProfileComponentF from './components/UserProfileComponentF';
 import UserProfileComponentC from './components/UserProfileComponentC';
@@ -7,15 +7,78 @@ import TestFlexbox from './components/TestFlexbox';
 import TestLayout from './components/TestLayout';
 
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const HomePageScreen = props => {
+  const {navigation} = props;
+
+  return (
+    <View style={{backgroundColor: 'pink', flex: 1}}>
+      <TestLayout />
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Details');
+        }}
+        style={{height: 50, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Navigate Details Screen</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.push('Details');
+        }}
+        style={{height: 50, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Push Details Screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const DetailsScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>this is a details screen</Text>
+      <Button
+        title={'Navigate to yet another Details screen'}
+        onPress={() => {
+          navigation.navigate('Details');
+        }}
+      />
+
+      <Button
+        title={'Push to yet another Details screen'}
+        onPress={() => {
+          navigation.push('Details');
+        }}
+      />
+
+      <Button
+        title={'Push to yet another Home screen'}
+        onPress={() => {
+          navigation.push('Home');
+        }}
+      />
+
+      <Button
+        title={'Navigate to yet another Home screen'}
+        onPress={() => {
+          navigation.navigate('Home');
+        }}
+      />
+    </View>
+  );
+};
 
 const App = () => {
   const [parentFirstName, setParentFirstName] = useState('');
 
   return (
     <NavigationContainer>
-      <View style={{backgroundColor: 'pink', flex: 1}}>
-        <TestLayout />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomePageScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
