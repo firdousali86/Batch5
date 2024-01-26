a52ebaa6c2f59237c6136af2c62d32b9c33f6d25
Firdous Ali <firdous.ali.tp@gmail.com>
2024-01-26 18:48:12 +0500

    refactor

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './navigation';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/navigation/index.js
@@ -0,0 +1,27 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {HomeScreen, DetailsScreen} from '../screen';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        initialParams={{
+          sessionName: 'some initial value of session name',
+          BatchNumber: 0,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default Navigation;
+
+const styles = StyleSheet.create({});
