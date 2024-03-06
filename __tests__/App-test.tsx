0b444710e3027c11c0a6635b4c4bc513cdfc98f6
Firdous Ali <firdous.ali.tp@gmail.com>
2024-03-06 18:54:09 +0500

    working jest

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -9,9 +9,43 @@ import {it} from '@jest/globals';
 import renderer from 'react-test-renderer';
 
 import {render, fireEvent} from '@testing-library/react-native';
+import {validatePassword} from '../src/utils';
 // it('renders correctly', () => {
 //   renderer.create(<App />);
 // });
+
+test('case 1', () => {
+  const password = '1234';
+
+  const result = validatePassword(password);
+
+  expect(result).toBe(false);
+});
+
+test('case 2', () => {
+  const password = '123456789';
+
+  const result = validatePassword(password);
+
+  expect(result).toBe(false);
+});
+
+test('case 3', () => {
+  const password = 'We868r6$';
+
+  const result = validatePassword(password);
+
+  expect(result).toBe(true);
+});
+
+test('case 4', () => {
+  const password = 'We868r6hfh4545';
+
+  const result = validatePassword(password);
+
+  expect(result).toBe(false);
+});
+
 test('test my react component', () => {
   const {getByTestId} = render(<ReactTestingComponent />);
   const mylabel = getByTestId('mytextlabel');
